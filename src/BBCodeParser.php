30c958d3fdf5112591f983ec63f75102320dc704
crotanite <crotanite@lumitestudios.com>
2025-05-10 14:43:46 +0100

    feat: optionally allow passing parsers to the method

--- src/BBCodeParser.php
+++ src/BBCodeParser.php
@@ -6,20 +6,24 @@ use LumiteStudios\BBCodeParser\Contracts\Parser;
 
 class BBCodeParser
 {
-    public static function parse(string $content)
+    public static function parse(string $content, ?array $parsers = null)
     {
         if(config('bbcode-parser.break-newlines')) {
             $content = Str::replaceMatches('/ul.*?\/ul(*SKIP)(*F)|[\n]/s', fn ($matches) => '<br>', $content);
         }
-        self::getParsers()->each(function ($parser) use (&$content) {
+
+        $_parsers = self::getParsers($parsers);
+
+        $_parsers->each(function ($parser) use (&$content) {
             $content = Str::replaceMatches($parser::pattern(), fn ($matches) => $parser::match($matches), $content);
         });
+
         return $content;
     }
 
-    protected static function getParsers()
+    protected static function getParsers(?array $parsers = null)
     {
-        return collect(config('bbcode-parser.parsers'))
+        return collect($parsers ?? config('bbcode-parser.parsers'))
             ->transform(fn ($parser) => new $parser)
             ->filter(fn ($parser) => $parser instanceof Parser);
     }
