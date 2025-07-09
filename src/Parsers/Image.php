f3c28906e4978bc2c6b52dd70bd127d59b819260
crotanite <crotanite@lumitestudios.com>
2025-07-09 17:42:52 +0100

    fix: update image parser to not default to pixels

--- src/Parsers/Image.php
+++ src/Parsers/Image.php
@@ -15,8 +15,8 @@ class Image implements Parser
     public static function match($matches): string
     {
         $options = Str::matchPattern('/(height|width|alt|title)=((".*?")|(\S*))/s', $matches[2], '"');
-        $height = array_key_exists('height', $options) ? $options['height'].'px' : 'auto';
-        $width = array_key_exists('width', $options) ? $options['width'].'px' : 'auto';
+        $height = array_key_exists('height', $options) ? $options['height'] : 'auto';
+        $width = array_key_exists('width', $options) ? $options['width'] : 'auto';
         $src = $matches[3];
         $alt = array_key_exists('alt', $options) ? $options['alt'] : $src;
         $title = array_key_exists('title', $options) ? $options['title'] : $src;
